826765311d435cce8a828366721f76b7b9ed26da
chillee <horacehe2007@yahoo.com>
2016-08-10 01:24:37 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path    = require('path');
+var request = require('request')
+
+var url = 'https://www.reddit.com/r/all/top/.json?before=t3_1juwvl&t=all'
+
+
+
+var app = express();
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/test.html'));
+})
+
+var server = app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+//https://www.reddit.com/r/all/top/?before=t3_1juwvl&t=all
